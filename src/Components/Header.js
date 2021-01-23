c9d34aea763f26b5cad418a334d8435125da6510
DoubleS-Lee <lss8825@gmail.com>
2021-01-23 13:58:24 +0900

    3.2:(local-css)styled-components로_css적용하는방법(각_태그_이름을_바꿔주면서_스타일을_적용하는_방법)제일_강추

---
+++ src/Components/Header.js
@@ -0,0 +1,36 @@
+// yarn add styled-components
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const SHeader = styled.header``;
+
+const List = styled.ul`
+    display: flex;
+    &:hover {
+        background-color: blue;
+    }
+`;
+
+const Item = styled.li``;
+
+const SLink = styled(Link)``;
+
+
+export default function Header () {
+    return (
+        <SHeader>
+            <List>
+                <Item>
+                    <SLink to='/'>Movies</SLink>
+                </Item>
+                <Item>
+                    <SLink to='/tv'>TV</SLink>
+                </Item>
+                <Item>
+                    <SLink to='/search'>Search</SLink>
+                </Item>
+            </List>
+        </SHeader>
+    )
+}
--- src/Components/Header/index.js
+++
@@ -1,4 +0,0 @@
-// index로 찾아왔을때 폴더 내부에 있는 진짜 js 파일로 보내기 위한 코드
-import Header from './Header';
-
-export default Header;
