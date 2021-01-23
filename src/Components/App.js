7169625b5bad9733c5bc7e7ae96fd547356cc7de
DoubleS-Lee <lss8825@gmail.com>
2021-01-23 11:52:34 +0900

    3.0:파일경로를_폴더로_설정했을때_index.js_파일로찾는법,js파일내에서_css파일불러오기

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// Header를 호출할때 Header가 폴더이면 폴더 안에 있는 index.js 파일을 찾아들어가서 호출을 한다
+// 따라서 .js 파일이 아니라 폴더로 만들때는 폴더 내에 index.js 파일을 심어줘야 한다
 import Header from './Header';
 import Router from './Router';
 
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Header.css'
 
 export default function Header () {
     return (
-        <ul>
+        <ul className='nav'>
             <li>
                 <a href="/">Movies</a>
             </li>
