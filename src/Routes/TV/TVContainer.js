36c25406969a81e7a3c9f7489b6cdada60bf849b
DoubleS-Lee <lss8825@gmail.com>
2021-01-24 11:45:41 +0900

    5.3:TVContainer.js_제작

--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { tvApi } from '../../api';
 import TVPresenter from './TVPresenter';
 
 export default function TVContainer() {
@@ -9,6 +10,34 @@ export default function TVContainer() {
         error: null,
         loading: true,
     });
+
+    const getApi = async() => {
+        try{
+            const {data:{results:topRated}} = await tvApi.topRated();
+            // console.log(topRated);
+            const {data:{results:popular}} = await tvApi.popular();
+            const {data:{results:airingToday}} = await tvApi.airingToday();
+            setState({
+                topRated:{topRated},
+                popular:{popular},
+                airingToday:{airingToday},
+            });
+        } catch {
+            setState({
+                error:"TV 정보를 불러올 수 없습니다"
+            });
+        } finally {
+            setState({
+                loading:false,
+            });
+        }
+        
+    }
+
+    useEffect(() => {
+        getApi();
+
+    })
 
     const { topRated, popular, airingToday, error, loading } = state;
     return(
